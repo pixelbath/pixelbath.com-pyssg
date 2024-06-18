320e0a22ae785f9ef19d1de2fb64ec1be7102b9a
pixelbath <mhoskins@pixelbath.com>
2024-06-18 16:22:07 -0600

    change to python markdown, fix captions

--- build.py
+++ build.py
@@ -1,20 +1,31 @@
 
 import os
 import pathlib
-import cmarkgfm
-from cmarkgfm.cmark import Options as cmarkgfmOptions
+import markdown
+import markdown.extensions.fenced_code
 import frontmatter
 import jinja2
 import highlighting
 from shutil import copy, copytree
 from pathlib import Path
 # import pixelbathdark
+from pprint import pprint
+from bs4 import BeautifulSoup
 
 sources = pathlib.Path('.').glob('src/posts/*.md')
 output_folder = './output'
 output_static = output_folder + '/static'
 output_images = output_folder + '/images'
-webroot = ''
+webroot = '../..'
+
+markdown_ = markdown.Markdown(
+    extensions=[
+        "toc",
+        "tables",
+        "abbr",
+        "fenced_code",
+    ]
+)
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('templates'),
@@ -27,18 +38,19 @@ if not os.path.exists(output_static):
 if not os.path.exists(output_images):
     os.makedirs(output_images)
 
-for source in sources:
-    post = frontmatter.load(str(source))
-    content = cmarkgfm.markdown_to_html_with_extensions(
-        post.content,
-        extensions=['table', 'autolink'],
-        options=cmarkgfmOptions.CMARK_OPT_UNSAFE
-    )
-    # set up post path
-    Path("{}/{}/".format(output_folder, source.stem)).mkdir(parents=True, exist_ok=True)
-
-    # highlight here
+def render_markdown(content: str) -> str:
+    markdown_.reset()
+    content = markdown_.convert(content)
     content = highlighting.highlight(content)
+    return content
+
+for source in sources:
+    print(f"Processing {source}")
+    post = frontmatter.load(str(source))
+
+    # set up post path
+    # Path("{}/{}/".format(output_folder, source.stem)).mkdir(parents=True, exist_ok=True)
+    content = render_markdown(post.content)
 
     # addtl pre-parsing of html
     post['stem'] = source.stem
@@ -47,8 +59,15 @@ for source in sources:
     rendered = template.render(post=post, content=content)
 
     # TODO: make this configurable
-    rendered = rendered.replace('"/images', '"{}/images'.format(webroot))
-    rendered = rendered.replace('"/static', '"{}/static'.format(webroot))
+    rendered = rendered.replace('../images', f"{webroot}/images")
+    rendered = rendered.replace('../static', f"{webroot}/static")
+
+    # update image captions
+    soup = BeautifulSoup(rendered, features='lxml')
+    captions = soup.find_all('div', {'class': 'image-caption'})
+    for caption in captions:
+        rendered = rendered.replace(caption.text.strip(), f"<p class=\"caption-text\">{caption.text.strip()}</p>")
+        # print(caption.text.strip())
     
     path.write_text(rendered, encoding="utf-8")
 
